83c17abe294e8d69ddc528edb514150f4b968c1a
Steve Lenz <sle@xima.de>
2017-10-26 16:25:46 +0200

    [Remove] ext_autoload.php

--- ext_autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-$extPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('simpleaddress');
-return array(
-    'Sle\TYPO3\Extbase\Controller\BaseController' => $extPath.'vendor/Sle/TYPO3/Extbase/Controller/BaseController.php',
-    'Sle\TYPO3\Extbase\Backend\PluginPreview'     => $extPath.'vendor/Sle/TYPO3/Extbase/Backend/PluginPreview.php',
-    'Sle\Google\Maps\MapsV3'                      => $extPath.'vendor/Sle/Google/Maps/MapsV3.php',
-    'Sle\Helper\StringHelper'                     => $extPath.'vendor/Sle/Helper/StringHelper.php',
-);
